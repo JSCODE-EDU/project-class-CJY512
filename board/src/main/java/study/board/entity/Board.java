e01fce450eea3feb16b72ddd8c2bc242e78df315
cjy <c0512jy@naver.com>
2023-05-10 20:39:01 +0900

    feat: 게시판 entity 추가

--- board/src/main/java/study/board/entity/Board.java
+++ board/src/main/java/study/board/entity/Board.java
@@ -1,2 +1,37 @@
-package study.board.entity;public class Board {
+package study.board.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    private Board(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Board createBoard(String title, String content) {
+        return new Board(title, content);
+    }
+
+    public Board update(Board newBoard) {
+        title = newBoard.getTitle();
+        content = newBoard.getContent();
+        return this;
+    }
 }
