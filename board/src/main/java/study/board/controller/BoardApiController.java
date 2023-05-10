dd8cc5915dbe4bf6106bada6f6b66c95dd4e3f18
cjy <c0512jy@naver.com>
2023-05-10 20:41:53 +0900

    feat: 게시판 CRUD와 검색 기능 추가

--- board/src/main/java/study/board/controller/BoardApiController.java
+++ board/src/main/java/study/board/controller/BoardApiController.java
@@ -1,7 +1,67 @@
 package study.board.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import study.board.controller.dto.BoardDto;
+import study.board.controller.dto.Result;
+import study.board.entity.Board;
+import study.board.repository.BoardRepository;
+import study.board.service.BoardService;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-public class BoardController {
+@RequiredArgsConstructor
+@RequestMapping("/api/boards")
+@ResponseStatus(HttpStatus.OK) //동적 설정 필요시 수정
+public class BoardApiController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public BoardDto saveBoard(@RequestBody BoardDto boardDto) {
+        return BoardDto.from(boardService.save(Board.createBoard(boardDto.getTitle(), boardDto.getContent())));
+    }
+
+    @GetMapping
+    public Result<BoardDto> findBoards() {
+        return new Result(boardService.findBoards().stream()
+                .map(BoardDto::from)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/search")
+    public Result<BoardDto> searchBoards(@RequestParam String query) {
+        return new Result(boardService.findByTitle(query).stream()
+                .map(BoardDto::from)
+                .collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public BoardDto findOne(@PathVariable("id") Long id) {
+        return BoardDto.from(boardService.findById(id));
+    }
+
+    @PostMapping("/{id}")
+    public BoardDto updateBoard(@PathVariable("id") Long id, @RequestBody BoardDto boardDto) {
+        Board updatedBoard = boardService.update(id, Board.createBoard(boardDto.getTitle(), boardDto.getContent()));
+        return BoardDto.from(updatedBoard);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteOne(@PathVariable("id") Long id) {
+        boardService.deleteById(id);
+        return "ok";
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 1; i < 300; i++) {
+            boardService.save(Board.createBoard("title" + i, "content" + i));
+        }
+    }
 }
--- board/src/main/java/study/board/service/BoardService.java
+++ board/src/main/java/study/board/service/BoardService.java
@@ -1,2 +1,51 @@
-package study.board.service;public class BoardService {
+package study.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import study.board.entity.Board;
+import study.board.exception.BoardNotFoundException;
+import study.board.repository.BoardRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Board save(Board board) {
+        boardRepository.save(board);
+        return board;
+    }
+
+    public List<Board> findBoards() {
+        return boardRepository.findTop100AllByOrderByCreatedDateDesc();
+    }
+
+    public Board findById(Long id) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("no such data"));
+    }
+
+    public List<Board> findByTitle(String title) {
+        return boardRepository.findTop100ByTitleContainingOrderByCreatedDateDesc(title);
+    }
+
+    @Transactional
+    public Board update(Long id, Board newBoard) {
+        return boardRepository.findById(id)
+                .orElseThrow(() -> new BoardNotFoundException("id" + id + "에 해당하는 게시판이 없습니다."))
+                .update(newBoard);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        boardRepository.delete(boardRepository.findById(id)
+                .orElseThrow(() -> new BoardNotFoundException(id + ", id에 해당하는 게시판이 없습니다.")));
+    }
+
 }
