0a2320492fa86bed053b57ce245d720004856c91
cjy <c0512jy@naver.com>
2023-05-10 20:38:36 +0900

    feat: 공통 생성시간 저장용 base entity 추가

---
+++ board/src/main/java/study/board/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package study.board.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+}
