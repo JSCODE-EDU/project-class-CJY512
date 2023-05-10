d67fd2e938c137518719316fc4e10a1f8d4be81e
cjy <c0512jy@naver.com>
2023-05-10 20:42:53 +0900

    feat: controller DTO 생성

--- board/src/main/java/study/board/controller/dto/BoardDto.java
+++ board/src/main/java/study/board/controller/dto/BoardDto.java
@@ -1,2 +1,26 @@
-package study.board.controller.dto;public class BoardDto {
+package study.board.controller.dto;
+
+import lombok.Data;
+import study.board.entity.Board;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BoardDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+
+    private BoardDto(Long id, String title, String content, LocalDateTime createdDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+    }
+
+    public static BoardDto from(Board board) {
+        return new BoardDto(board.getId(), board.getTitle(), board.getContent(), board.getCreatedDate());
+    }
 }
