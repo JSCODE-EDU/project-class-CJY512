0a66038e5bf4e169569b83448e1b299ad753905c
cjy <c0512jy@naver.com>
2023-05-11 21:51:36 +0900

    fix: 엔티티마다 Auto-increment 적용

--- board/src/main/java/study/board/entity/Board.java
+++ board/src/main/java/study/board/entity/Board.java
@@ -4,17 +4,15 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
     private Long id;
 
     private String title;
